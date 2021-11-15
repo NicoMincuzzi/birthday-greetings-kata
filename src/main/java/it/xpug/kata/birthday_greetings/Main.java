c975ba4ff7a86fd2e53ffcfc26770be1e3b09ac9
Nicola Mincuzzi <nicolamincuzzi88@gmail.com>
2021-11-15 23:08:09 +0100

    Remove warns show from IDE

--- src/main/java/it/xpug/kata/birthday_greetings/Main.java
+++ src/main/java/it/xpug/kata/birthday_greetings/Main.java
@@ -1,16 +1,12 @@
 package it.xpug.kata.birthday_greetings;
 
-import java.io.*;
+import javax.mail.MessagingException;
+import java.io.IOException;
 import java.text.ParseException;
 
-import javax.mail.*;
-import javax.mail.internet.*;
-
 public class Main {
-
-	public static void main(String[] args) throws AddressException, IOException, ParseException, MessagingException {
+	public static void main(String[] args) throws IOException, ParseException, MessagingException {
 		BirthdayService service = new BirthdayService();
 		service.sendGreetings("employee_data.txt", new XDate(), "localhost", 25);
 	}
-
 }
