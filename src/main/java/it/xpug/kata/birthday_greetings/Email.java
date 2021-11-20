691c8ace1f8465c91ebc40ef7bb3c125f6f99d9c
Nicola Mincuzzi <nicolamincuzzi88@gmail.com>
2021-11-20 13:52:51 +0100

    Create Email class

---
+++ src/main/java/it/xpug/kata/birthday_greetings/Email.java
@@ -0,0 +1,28 @@
+package it.xpug.kata.birthday_greetings;
+
+import java.util.Objects;
+
+public class Email {
+    private final String address;
+
+    public Email(String address) {
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Email email = (Email) o;
+        return Objects.equals(address, email.address);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(address);
+    }
+}
